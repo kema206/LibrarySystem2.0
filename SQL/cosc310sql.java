1d309d3b8b21ef010547a318e7976f9722bfbd02
leminerale04 <77290006+leminerale04@users.noreply.github.com>
2022-11-10 19:10:53 -0800

    created .yml file

---
+++ SQL/cosc310sql.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class cosc310sql
+{
+    public static void main(String[] args)
+	{	String url = "jdbc:mysql://localhost:3307/workson";
+		String uid = "root";
+		String pw = "304rootpw";
+
+		try 
+		(Connection con=DriverManager.getConnection(url, uid, pw);
+		Statement stmt = con.createStatement(); )
+		{
+			// String sql = "SELECT * FROM users";
+			// ResultSet rst = stmt.executeQuery(sql);
+
+			// StringBuilder output = new StringBuilder(); //for printing
+			// output.append("uid, uname, pwd, lvl, book1, book2");
+
+			// while (rst.next())
+			// {
+			// 	output.append("\n"+rst.getInt("uid")+", "+rst.getString("uname"));
+			// }
+			System.out.println("connected");
+		}
+		catch (SQLException ex)
+		{
+			System.out.println("SQLException: " + ex);
+		}		
+	}
+}
